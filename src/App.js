28e2bc5f17589323c1f239aa18c00c0e37661b28
Sarah Paz <sarah.paz@gmail.com>
2019-10-18 13:55:04 -0400

    change functional component into a class component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//* Class component gives access to State
+class App extends Component {
+  constructor() {
+    super(); //* This calls the constructor method on the component class and gives access to this.state
+
+    this.state = {
+      monsters: [
+        {
+          name: "Frankenstein"
+        },
+        {
+          name: "Dracula"
+        },
+        {
+          name: "Zombie"
+        }
+      ]
+    };
+  }
+
+  //* As soon as state changes we re-render the new component with the new state and display it
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster, index) => (
+          //* Unique key is required because React needs to know which element it needs to update *
+          <h1 key={index}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
