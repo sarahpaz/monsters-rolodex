5a5260333c648f5aa90c7775ace94e36c97b29a8
Sarah Paz <sarah.paz@gmail.com>
2019-10-18 18:26:39 -0400

    add search / filter feature

--- src/App.js
+++ src/App.js
@@ -18,21 +18,27 @@ class App extends Component {
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
-      .then(users => this.setState({ monsters: users }));
+      .then(users => this.setState({ monsters: users })); //* Everytime you call setState it re-renders
   }
 
   //* As soon as state changes we re-render the new component with the new state and display it
   render() {
+    const { monsters, searchField } = this.state;
+
+    const filteredMonsters = monsters.filter(
+      monster =>
+        monster.name.toLowerCase().includes(searchField.toLowerCase()) ||
+        monster.email.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
         <input
           type="search"
           placeholder="search monsters"
-          onChange={e => {
-            this.setState({ search: e.target.value });
-          }}
+          onChange={e => this.setState({ searchField: e.target.value })}
         />
-        <CardList monsters={this.state.monsters} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
