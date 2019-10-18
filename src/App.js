7d4fa6ec8fb3d496ffb3c9059a1ff6de3b95f66e
Sarah Paz <sarah.paz@gmail.com>
2019-10-18 16:23:12 -0400

    import CardList and return CardList

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
 
 //* Class component gives access to State
 class App extends Component {
@@ -7,28 +8,22 @@ class App extends Component {
     super(); //* This calls the constructor method on the component class and gives access to this.state
 
     this.state = {
-      monsters: [
-        {
-          name: "Frankenstein"
-        },
-        {
-          name: "Dracula"
-        },
-        {
-          name: "Zombie"
-        }
-      ]
+      monsters: []
     };
+  }
+
+  //* Mount is when React puts our component on the page, renders it on the DOM for the first time, it calls the block of code inside*//
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
 
   //* As soon as state changes we re-render the new component with the new state and display it
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster, index) => (
-          //* Unique key is required because React needs to know which element it needs to update *
-          <h1 key={index}>{monster.name}</h1>
-        ))}
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
