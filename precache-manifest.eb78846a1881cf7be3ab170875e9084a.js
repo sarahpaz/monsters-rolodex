aac63b4641af705f0620ca0e042065de5104ca48
Sarah Paz <sarah.paz@gmail.com>
2019-10-19 18:18:10 -0400

    Updates

---
+++ precache-manifest.eb78846a1881cf7be3ab170875e9084a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "133c3e94cbcff8ced433dd6fda727105",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ad1b6d8d69fc8a49ea51",
+    "url": "/monsters-rolodex/static/css/main.74433981.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "/monsters-rolodex/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "ad1b6d8d69fc8a49ea51",
+    "url": "/monsters-rolodex/static/js/main.379f0669.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
